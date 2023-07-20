03a3bc7d20bda809c3eae63668f919b5a05c1929
linuxscout <taha_zerrouki@hotmail.com>
2023-07-20 13:26:16 +0100

    fix erros in inflection format in Data

--- tests/test_sampledb.py
+++ tests/test_sampledb.py
@@ -30,7 +30,7 @@ class TestSampleDBCase(unittest.TestCase):
         print("index size ",len(word_index.keys()))
         self.assertEqual(len(word_index.get("",[])), 0, " Index contains Empty words")  # add assertion here
         self.assertEqual(len(non_arabic_words), 0, " Index contains non Arabic word")  # add assertion here
-        self.assertEqual(len(word_index), 3176, " Index size changed")  # add assertion here
+        self.assertEqual(len(word_index), 3172, " Index size changed")  # add assertion here
 
     def test_temp_create_index(self):
         """
@@ -59,7 +59,7 @@ class TestSampleDBCase(unittest.TestCase):
         print("index size ",len(word_index.keys()))
         self.assertEqual(len(word_index.get("",[])), 0, " Index contains Empty words")  # add assertion here
         self.assertEqual(len(non_arabic_words), 0, " Index contains non Arabic word")  # add assertion here
-        self.assertEqual(len(word_index), 3176, " Index size changed")  # add assertion here
+        self.assertEqual(len(word_index), 3172, " Index size changed")  # add assertion here
 
     # @unittest.skip("to do")
     def test_match_index(self):
@@ -89,6 +89,60 @@ class TestSampleDBCase(unittest.TestCase):
         print("--- Duplicated cases ---")
         pprint.pprint(dups)
         self.assertEqual(len(dups), 0, "There are duplicata")  # add assertion here
+
+
+    # @unittest.skip("to do")
+    def test_check_inflection(self):
+        """
+        test create index
+        """
+        sm = yarob.samples_const.SAMPLES
+        wrongs = []
+        cpt = 0
+        for id1 in sm:
+            inflects_lines =  sm[id1]["inflection"].strip().split(".")
+            # if len(inflects_lines)>=2  and inflects_lines[0].strip() == inflects_lines[1].strip():
+            #     inflects_lines = inflects_lines[1:]
+            warn = ""
+            new_lines = []
+            for i, line in enumerate(inflects_lines):
+                line = line.strip()
+                parts = line.split(":")
+                part0 = parts[0]
+                remain_parts = ":".join(parts[1:])
+                if ":" in line:
+                    if len(part0) > len(remain_parts):
+                       if  (not line.startswith("وجملة")
+                         and not line.startswith("والجملة")
+                         and not line.startswith("وشبه الجملة")
+                         and not line.startswith("والمصدر المؤول")
+                         ):
+                            warn = "Line '%d':key '%s' is too long"%(i, part0)
+                            wrongs.append((id1, warn))
+                elif not line:
+                    pass
+                elif (not line.startswith("وجملة")
+                      and not line.startswith("والجملة")
+                      and not line.startswith("وشبه الجملة")
+                      and not line.startswith("والمصدر المؤول")
+                ):
+                    warn = " Line %d: start error [%s]" %(i,line)
+                    wrongs.append((id1, warn))
+                    line = "TOJOIN"+line
+                else:
+                    pass
+                    # line = "###" +line
+                new_lines.append(line)
+                text  = ".\n".join(new_lines)
+                # join lines
+                text = text.replace(".\nTOJOIN", "")
+                sm[id1]["inflection"] = text
+            # pprint.pprint(new_lines)
+
+        pprint.pprint(wrongs)
+        # pprint.pprint(sm, width=500)
+        # print(sm)
+        self.assertEqual(len(wrongs), 0, "There are wrong lines inflection")  # add assertion here
 
 
 if __name__ == '__main__':
